7ce6254f342f1f1155624ae701bd1d3d8e093eb2
ASHRAFSHAIK3 <137259937+ASHRAFSHAIK3@users.noreply.github.com>
2026-03-26 17:19:23 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,21 +1,26 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || '/api'
+  baseURL: `${import.meta.env.VITE_API_URL}/api`
 })
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token')
-  if (token) config.headers.Authorization = `Bearer ${token}`
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
   return config
 })
 
-api.interceptors.response.use(r => r, err => {
-  if (err.response?.status === 401) {
-    localStorage.removeItem('token')
-    window.location.href = '/login'
+api.interceptors.response.use(
+  res => res,
+  err => {
+    if (err.response?.status === 401) {
+      localStorage.removeItem('token')
+      window.location.href = '/login'
+    }
+    return Promise.reject(err)
   }
-  return Promise.reject(err)
-})
+)
 
 export default api
